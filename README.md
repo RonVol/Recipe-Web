b431200c736dd01ee64a76f5f740cbaaf924dba0
RonVol <103151691+RonVol@users.noreply.github.com>
2024-08-30 15:24:02 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# Recipes Website
+This is a web application that allows users to search for and explore various recipes.
+
+## Introduction
+The Recipes project is a recipe search system designed to provide users with access to a diverse database of recipes. Users can view recipe details, preparation instructions, and even save their favorite family recipes. The project is divided into frontend and backend components, each fulfilling specific requirements set by the client.
+The backend of the Recipes project serves as the foundation for the entire application. It handles data storage, retrieval, authentication, and other crucial operations required to support the frontend functionalities.
+
+## Technology Stack
+Frontend: Vue.js
+Backend: Express.js (Node.js)
+Database: MySQL
+Authentication: JSON Web Tokens (JWT)
+External data source: Spoonacular API
+
+![homepage-img](images/homepage-img.jpg "homepage-img")<br>
+
+## Backend API
+![api-img](images/api-img.jpg "api-img")<br>
---
+++ backend/routes/recipes.js
@@ -0,0 +1,61 @@
+var express = require("express");
+var router = express.Router();
+const recipes_utils = require("./utils/recipes_utils");
+
+//let randoms = []
+
+router.get("/random", async (req, res) => {
+  // if(randoms.length!= 0){
+  //   res.send(randoms);
+  //   return
+  // }
+  try {
+    const recipes = await recipes_utils.getAllRecapiesss();
+    randoms = recipes;
+    console.log(recipes)
+    res.send(recipes);//send to client all recipes
+  } catch (error) {
+    res.send(error)
+  }
+});
+
+/**
+ * This path returns a full details of a recipe by its id
+ */
+router.get("/:recipeId", async (req, res, next) => {
+  try {
+    const recipe = await recipes_utils.getRecipeDetails(req.params.recipeId);
+    res.send(recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+
+/**
+ * This path returns a details of a recipe by its query
+ */
+router.post("/search", async (req, res, next) => {
+  try {
+    const recipe = await recipes_utils.getRecipeBySearch(req.body.search,
+      parseInt(req.body.limit), req.body.cuisine, req.body.diet, req.body.intolerance);
+    
+    res.send(recipe);
+  } catch (error) {
+    next(error);
+  }
+});
+/**
+ * This path returns a recipe's likes by its id
+ */
+router.get("/recipe-popularity/:id", async (req, res, next) => {
+  try {
+    const recipePopularity = await recipes_utils.getRecipePopularity(req.params.id);
+    console.log("recipePopularity -> " + recipePopularity)
+    res.send(recipePopularity.toString());
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = router;
